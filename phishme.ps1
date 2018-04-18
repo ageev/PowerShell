affe3e580768dc38f400ca71b63fdddc61cbae01
Artem <root@itsec.pro>
2018-04-18 17:25:40 +0200

    Create phishme.ps1

---
+++ phishme.ps1
@@ -0,0 +1,26 @@
+Write-Output "================= PhishMe Search Script v0.1 ===================="
+Write-Output " (c) Artyom Ageyev"
+Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null 
+$olFolders = "Microsoft.Office.Interop.Outlook.olDefaultFolders" -as [type]  
+$outlook = new-object -comobject outlook.application 
+$namespace = $outlook.GetNameSpace("MAPI") 
+$folder = $namespace.Folders("phishreporter@canon-europe.com").Folders("Inbox")
+$date = (Get-date).AddHours(-24)
+Write-Output "$("Working with emails from") $date"
+$search_string = Read-Host "Enter the search string:"
+$mails = $folder.items | where-object { $_.ReceivedTime -gt $date}  
+# $mails = $folder.items | where-object { $_.ReceivedTime -gt [DateTime]::ParseExact($date, 'd/M/yyyy HH:mm:ss',[CultureInfo]::InvariantCulture)} 
+
+$outputdir = "C:\Temp\phishme\"
+
+foreach($mail in $mails){
+
+$tag = ($mail.HTMLBody) -split "`n" | sls $search_string
+
+if ($tag) 
+    {
+    $filename = $mail.attachments(1).FileName
+    Write-Output "$("[INFO] Saving mail ") $filename)"
+    $mail.attachments(1).saveasfile($outputdir + $filename)
+    }
+}
