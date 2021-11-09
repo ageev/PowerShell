b22e75ba0d69d9b84cf7cfae59d077929c58670a
Artyom <root@itsec.pro>
2021-11-09 12:05:26 +0100

    Add files via upload

---
+++ get_lastlogondate.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ActiveDirectory
+
+#$PropertyName = "PasswordLastSet"
+$PropertyName = "LastLogonDate"
+$Username = "Grefen, Hans"
+
+function Msg ($Txt="") { 
+    Write-Host "$([DateTime]::Now) $Txt" 
+}
+
+$List = @() #Define Array 
+(Get-ADDomain).ReplicaDirectoryServers | Sort | % { 
+    $DC = $_ 
+#    Msg "Reading $DC" 
+    Get-ADUser -Server $_ -Filter "displayname -like '$Username'" -Properties $PropertyName | Select samaccountname,$PropertyName,@{n='DC';e={$DC}} 
+} 
+
+Write-Host -NoNewLine 'Press any key to close...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
